007ed8c2111df196885ae06b2ff6188543fb8c84
user <maulananurrohman07.com>
2019-01-21 10:27:14 +0700

    Belajar Commparison Operator

--- index.js
+++ index.js
@@ -104,11 +104,25 @@
 
 //Comparison Operators//
 
+// let x = 1;
+
+// // Relational
+// console.log(x > 0);
+// console.log(x >= 1);
+// console.log(x < 1);
+// console.log(x <= 1);
+
+// // Equality
+// console.log(x === 1);
+// console.log(x !== 1);
+
+// Comparison Operators //
+
 let x = 1;
 
 // Relational
 console.log(x > 0);
-console.log(x >= 1);
+console.log(x >= 1); 
 console.log(x < 1);
 console.log(x <= 1);
 
